f0503b66c574f8d61dea3d39d29981ecd0cc14af
Akagi201 <akagi201@gmail.com>
2014-12-01 10:24:54 +0800

    remove errno in lwlog_info, use lwlog_info for important info prints

--- lwlog.h
+++ lwlog.h
@@ -68,7 +68,7 @@
 #define lwlog_err(M, ...)     fprintf(stderr, BROWN  "[ERR]     " "%s (%s:%d) " NONE M YELLOW " errno: %s\n" NONE, __func__, __FILE__, __LINE__, ##__VA_ARGS__, clean_errno());
 #define lwlog_warning(M, ...) fprintf(stderr, GREEN  "[WARNING] " "%s (%s:%d) " NONE M YELLOW " errno: %s\n" NONE, __func__, __FILE__, __LINE__, ##__VA_ARGS__, clean_errno());
 #define lwlog_notice(M, ...)  fprintf(stderr, CYAN   "[NOTICE]  " "%s (%s:%d) " NONE M YELLOW " errno: %s\n" NONE, __func__, __FILE__, __LINE__, ##__VA_ARGS__, clean_errno());
-#define lwlog_info(M, ...)    fprintf(stderr, BLUE   "[INFO]    " "%s (%s:%d) " NONE M YELLOW " errno: %s\n" NONE, __func__, __FILE__, __LINE__, ##__VA_ARGS__, clean_errno());
+#define lwlog_info(M, ...)    fprintf(stderr, BLUE   "[INFO]    " "%s (%s:%d) " NONE M "\n", __func__, __FILE__, __LINE__, ##__VA_ARGS__);
 #define lwlog_debug(M, ...)   fprintf(stderr, GRAY   "[DEBUG]   " "%s (%s:%d) " NONE M "\n", __func__, __FILE__, __LINE__, ##__VA_ARGS__);
 
 /* LOG_LEVEL controls */
